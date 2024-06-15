4a66d284b87af321c35696d729031f5a1e650956
Денис Касперович <kasperovich.deonis@gmail.com>
2024-06-15 14:57:28 +0300

    Mapping many

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Country} from "./Country";
+import {v1} from "uuid";
+
+export type BanknotsType = 'USD' | 'RUB' | 'All'
+export type MoneyType = {
+    banknote: BanknotsType
+    nominal: number
+    id: string
+}
+
+let defaultMoney: Array<MoneyType> = [  // типизируем
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'RUB', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'RUB', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'RUB', nominal: 100, id: v1()},
+]
+
+
+export const moneyFilter = (money: Array<MoneyType>, filter: BanknotsType): Array<MoneyType> => {
+    if (filter === 'All') return money
+    return money.filter(m => m.banknote === filter)
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState<Array<MoneyType>>(defaultMoney)
+    const [filterValue, setFilterValue] = useState<BanknotsType>('All')
+    const filteredMoney: MoneyType[] = moneyFilter(money, filterValue)
+
+    const addMoney = (banknote: BanknotsType) => {
+        const newBanknote = {banknote: banknote, nominal: 100, id: v1()}
+        setMoney([...money, newBanknote])
+    }
+
+    const removeMoney = (banknote: BanknotsType) => {
+       const index = money.findIndex(b => b.banknote === banknote)
+        if (index !== -1) {
+            setMoney(money.filter((el, i) => i !== index));
+        }
+    }
+
+    return (
+        <div className="App">
+            <Country
+                data={filteredMoney}
+                setFilterValue={setFilterValue}
+                addMoney={addMoney}
+                removeMoney={removeMoney}
+            />
+        </div>
+    );
 }
 
 export default App;
